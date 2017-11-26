44902cdfbde2762f42e687946e352e89a4faf439
Klaus Löffelmann <klaus@loeffelmann.de>
2017-11-26 13:42:17 +0100

    Update First UserInterface-Property Unit Tests.

--- src/Compilers/VisualBasic/Test/Emit/CodeGen/CodeGenUserInterfaceProperties.vb
+++ src/Compilers/VisualBasic/Test/Emit/CodeGen/CodeGenUserInterfaceProperties.vb
@@ -25,28 +25,73 @@ End Class
                 VerifyIL("UserInterfaceOnProperty.set_FooProp",
             <![CDATA[
 {
-  // Code size       60 (0x3c)
+  // Code size       49 (0x31)
   .maxstack  3
-  .locals init (System.EventHandler V_0) //del
-  IL_0000:  ldsfld     "MyClass1._Closure$__.$I0-0 As System.EventHandler"
-  IL_0005:  brfalse.s  IL_000e
-  IL_0007:  ldsfld     "MyClass1._Closure$__.$I0-0 As System.EventHandler"
-  IL_000c:  br.s       IL_0024
-  IL_000e:  ldsfld     "MyClass1._Closure$__.$I As MyClass1._Closure$__"
-  IL_0013:  ldftn      "Sub MyClass1._Closure$__._Lambda$__0-0(Object, System.EventArgs)"
-  IL_0019:  newobj     "Sub System.EventHandler..ctor(Object, System.IntPtr)"
-  IL_001e:  dup
-  IL_001f:  stsfld     "MyClass1._Closure$__.$I0-0 As System.EventHandler"
-  IL_0024:  stloc.0
-  IL_0025:  ldstr      "qq"
-  IL_002a:  call       "Function System.AppDomain.CreateDomain(String) As System.AppDomain"
-  IL_002f:  dup
-  IL_0030:  ldloc.0
-  IL_0031:  callvirt   "Sub System.AppDomain.add_DomainUnload(System.EventHandler)"
-  IL_0036:  call       "Sub System.AppDomain.Unload(System.AppDomain)"
-  IL_003b:  ret
+  .locals init (System.ComponentModel.PropertyChangedEventHandler V_0)
+  IL_0000:  ldarg.0
+  IL_0001:  ldfld      "UserInterfaceOnProperty._FooProp As String"
+  IL_0006:  ldarg.1
+  IL_0007:  call       "Function Object.Equals(Object, Object) As Boolean"
+  IL_000c:  brtrue.s   IL_0030
+  IL_000e:  ldarg.0
+  IL_000f:  ldarg.1
+  IL_0010:  stfld      "UserInterfaceOnProperty._FooProp As String"
+  IL_0015:  ldarg.0
+  IL_0016:  ldfld      "UserInterfaceOnProperty.PropertyChangedEvent As System.ComponentModel.PropertyChangedEventHandler"
+  IL_001b:  stloc.0
+  IL_001c:  ldloc.0
+  IL_001d:  brfalse.s  IL_0030
+  IL_001f:  ldloc.0
+  IL_0020:  ldarg.0
+  IL_0021:  ldstr      "FooProp"
+  IL_0026:  newobj     "Sub System.ComponentModel.PropertyChangedEventArgs..ctor(String)"
+  IL_002b:  callvirt   "Sub System.ComponentModel.PropertyChangedEventHandler.Invoke(Object, System.ComponentModel.PropertyChangedEventArgs)"
+  IL_0030:  ret
 }
 ]]>)
         End Sub
+
+        <Fact()>
+        Public Sub SimpleUserInterfacePropertyUsingOnNotifyChanged()
+            CompileAndVerify(
+    <compilation>
+        <file name="a.vb">
+Imports System.ComponentModel
+
+Public Class UserInterfaceOnProperty
+    Implements INotifyPropertyChanged
+
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+    
+    Protected Overridable Sub OnPropertyChanged(eArgs as PropertyChangedEventArgs)
+        RaiseEvent PropertyChanged(me,eArgs)
+    End Sub
+
+    Public UserInterface Property FooProp As String
+End Class
+        </file>
+    </compilation>).
+                VerifyIL("UserInterfaceOnProperty.set_FooProp",
+            <![CDATA[
+{
+  // Code size       38 (0x26)
+  .maxstack  2
+  IL_0000:  ldarg.0
+  IL_0001:  ldfld      "UserInterfaceOnProperty._FooProp As String"
+  IL_0006:  ldarg.1
+  IL_0007:  call       "Function Object.Equals(Object, Object) As Boolean"
+  IL_000c:  brtrue.s   IL_0025
+  IL_000e:  ldarg.0
+  IL_000f:  ldarg.1
+  IL_0010:  stfld      "UserInterfaceOnProperty._FooProp As String"
+  IL_0015:  ldarg.0
+  IL_0016:  ldstr      "FooProp"
+  IL_001b:  newobj     "Sub System.ComponentModel.PropertyChangedEventArgs..ctor(String)"
+  IL_0020:  callvirt   "Sub UserInterfaceOnProperty.OnPropertyChanged(System.ComponentModel.PropertyChangedEventArgs)"
+  IL_0025:  ret
+}
+]]>)
+        End Sub
+
     End Class
 End Namespace
